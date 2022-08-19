91fd67a2b782f8873349a3385d035cb5654a17e8
utkuefeakdogan <utkuefeakdogan@gmail.com>
2022-08-19 16:41:13 +0300

    test changes

--- test/MyProject.ts
+++ test/MyProject.ts
@@ -2,81 +2,30 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { MyProject, MyProject__factory } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("MyProject", function(){
-  async function deploy() {
-    const [owner, otherAccount] = await ethers.getSigners();
-    const MyProject = await ethers.getContractFactory("MyProject");
-    const myproject = await MyProject.connect(owner).deploy();
-    return { myproject, owner, otherAccount };
-  }
+  let myproject:MyProject
+  let owner:SignerWithAddress
 
-  describe("Deployment from proper address", function(){
-    it("Should deployed from proper owner", async function () {
-      const { myproject, owner } = await loadFixture(deploy);
-      expect(myproject.address).to.be.properAddress;
-    });
+  before(async () => {
+    const signers = await ethers.getSigners();
+    owner = signers[0];
+  })
+  beforeEach(async () => {
+    myproject = await new MyProject__factory(owner).deploy();
+    await myproject.deployed();
+  })
+
+  it("Should deployed from the owner", async function () {
+    expect(await myproject.getOwner()).to.equal(owner.address);
   });
-
-  describe("AddFunctions", function(){
-    describe("addParent", function(){
-
-    });
-
-    describe("addChild", function(){
-
-    });
-  });
-
-  describe("GetFunctions", function(){
-    describe("getOwner", function(){
-
-    });
-
-    describe("getParent", function(){
-
-    });
-
-    describe("getChild", function(){
-
-    });
-
-    describe("get_All_Children", function(){
-
-    });
-
-    describe("get_All_Parents", function(){
-
-    });
-
-    describe("get_Children_Of_Parent", function(){
-
-    });
-
-    describe("get_Balance_of_Contract", function(){
-
-    });
-
-    describe("getRole", function(){
-
-    });
-  });
-
-  describe("delete_Child_With_ID", function(){
-    
-  });
-
-  describe("deposit_to_Child", function(){
-    
-  });
-
-  describe("WithdrawFunctions", function(){
-    describe("parent_Withdraws_Money", function(){
-
-    });
-
-    describe("child_Withdraws_Money", function(){
-
-    });
+  
+  it("Should add an unique parent ", async function () {
+    const parentInformation = await myproject.addParent("John","Doe","0xfE45fbb69d022b79b8A88694174c09232479a54D");
+    const allParents = await myproject.get_All_Parents();
+    const firstParent = allParents[0];
+    expect(firstParent.parentAddress).to.be.equal("0xfE45fbb69d022b79b8A88694174c09232479a54D"); 
   });
 });
