daf7086be728b554620f0321d0f79dc15b5148fc
utkuefeakdogan <utkuefeakdogan@gmail.com>
2022-08-31 10:24:37 +0300

    infura api_key eklendi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 
 
 const mnemonic = "provide crew bracket pottery bird jewel dismiss ripple clever phone wonder someone";
-const infura_api_key = "api_key";
-const infura_api_secret = "api_secret";
+const infura_api_key = "fcb9aa171b4f4fc1a600edf14953fcf9";
+const infura_api_secret = "2607faa084b849d2914f6ccd7cf2c55a";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
